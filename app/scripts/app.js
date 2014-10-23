14cc38e5e9e8ad78c3d4f555444666c2a10cc5bc
l-liava-l <legkodymov.lev@gmail.com>
2014-10-23 12:15:08 +0400

    Add employees

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,10 @@
-(function() {
-    angular.module('sochisat', [
+$(document).ready(function() {
 
-    ])
+    $('#b-work-with .label').hover(function(event) {
+        console.log(event);
+        $(event.target).tooltip('show');
+    });
 
+    $('body').scrollspy({ target: '.navbar-collapse' })
 
-
-})();
+});
